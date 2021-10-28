a57a7543fa6558bd455dd9f6d543f35c86d7a293
Amilcar Sanchez <amillcar.sanchez@wizeline.com>
2021-10-28 13:43:25 -0500

    app updated

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Navbar } from "./components/Navbar/Navbar";
+import { PokemonList } from "./container/PokemonList/PokemonList";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <PokemonList />
+    </>
+  );
 };
 
 export default App;
